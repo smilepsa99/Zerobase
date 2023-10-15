a2a0f0051a007e4cffe0a7206dde97e12954af35
smilepsa99 <smilepsa99@cau.ac.kr>
2023-10-15 11:38:37 +0900

    zb_15.1

--- src/week_06/Mission1_hw2.java
+++ src/week_06/Mission1_hw2.java
@@ -1,59 +1,60 @@
 package week_06;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Mission1_hw2 {
     public static void main(String[] args) {
-        System.out.println("[1단계] 나의 좌표를 입력해주세요");
-        int[] myPoint = makePoint();
+        int[] myPoint = makePoint("내 ");
 
-        System.out.println("[2단계] 임의의 좌표를 입력해주세요");
-        ArrayList<Integer> x_list = new ArrayList<>();
-        ArrayList<Integer> y_list = new ArrayList<>();
         ArrayList<int[]> points = new ArrayList<>();
         int len = 10;
         while (points.size() < len) {
-            int[] point = makePoint();
-            int x = point[0], y = point[1];
-            if (x_list.contains(x) && y_list.contains(y)) {
-                System.out.println("중복된 좌표입니다. 다시 입력해주세요");
+            System.out.printf("%d/%d 번째 입력\n", points.size() + 1, len);
+            int[] point = makePoint("임의의 ");
+            if (isContain(point, points)) {
+                System.out.println("동일한 좌표값이 이미 존재합니다. 다시 입력해주세요.");
             } else {
-                x_list.add(x);
-                y_list.add(y);
                 points.add(point);
-                System.out.printf("(%d, %d)가 입력됐습니다.[%d/10]\n", x, y, points.size());
             }
         }
 
-        int[] diffs = new int[len];
-        int min = -1;
+        double[] diffs = new double[len];
+        double min = -1;
         for (int i = 0; i < points.size(); i++) {
-            diffs[i] = Math.abs(myPoint[0]-points.get(i)[0])
-                    + Math.abs(myPoint[1]-points.get(i)[1]);
+            int a = Math.abs(myPoint[0]-points.get(i)[0]);
+            int b = Math.abs(myPoint[1]-points.get(i)[1]);
+            diffs[i] = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
+            System.out.printf("(%d, %d) => %f\n", points.get(i)[0], points.get(i)[1], diffs[i]);
             if (min == -1 || diffs[i] <= min) {
                 min = diffs[i];
             }
         }
 
-        StringBuilder answer = new StringBuilder();
+        System.out.println("제일 가까운 좌표:");
         for (int i = 0; i < diffs.length; i++) {
             if (diffs[i] == min) {
-                int x = points.get(i)[0];
-                int y = points.get(i)[1];
-                answer.append(String.format("(%d, %d) ", x, y));
+                System.out.printf("(%d, %d) => %f\n", points.get(i)[0], points.get(i)[1], diffs[i]);
             }
         }
-        System.out.println("[결과] 나와 가장 가까운 좌표값은 " + answer +  "입니다.");
+
     }
 
-    public static int[] makePoint() {
+    public static int[] makePoint(String s) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("x 좌표 값을 입력해주세요 : ");
-        int a = sc.nextInt();
-        System.out.print("y 좌표 값을 입력해주세요 : ");
-        int b = sc.nextInt();
-        int[] newPoint = {a, b};
+        System.out.println(s + "좌표 x값을 입력해 주세요.");
+        int x = sc.nextInt();
+        System.out.println(s + "좌표 y값을 입력해 주세요.");
+        int y = sc.nextInt();
+        int[] newPoint = {x, y};
         return newPoint;
+    }
+
+    public static boolean isContain(int[] point, ArrayList<int[]> points) {
+        for (int[] arr : points) {
+            if (Arrays.equals(arr, point)) return true;
+        }
+        return false;
     }
 }
